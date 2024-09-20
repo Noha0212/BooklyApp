e1bfcd97f49d2d14c9583a765709bf9bf70b0fec
Noha0212 <nonathabit2002@gmail.com>
2024-09-20 16:41:48 +0300

    create book model

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_list_view_item.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:bookly_app/features/search/presentation/views/widgets/search_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -24,24 +25,6 @@ class SearchViewBody extends StatelessWidget {
           Expanded(child: SearchResultListView())
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
-        );
-      },
     );
   }
 }
